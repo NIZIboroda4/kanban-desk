dc50da38d114436c7995d1703584953ddbc7ff86
NIZIboroda4 <niziborodach@mail.ru>
2022-01-04 12:11:57 +0300

    Co-authored-by: Aleksei Gudashev <Gudashev@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
+import AppBar from "components/app-bar/AppBar";
+import BoardColuwn from "components/board-column/BoardColuwn";
 import React from 'react';
-
-import logo from './logo.svg';
-import './App.css';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <AppBar/>
+      <BoardColuwn/>
+
     </div>
   );
 }
